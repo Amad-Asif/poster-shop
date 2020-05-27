553f7f233a34a0684d3480596728395ba849ee08
Amad Asif <amad.asif@securiti.ai>
2020-05-27 22:01:20 +0500

    test vue js app

--- server.js
+++ server.js
@@ -8,40 +8,53 @@ var bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
 app.get("/", function(req, res) {
-	res.sendFile(path.join(__dirname + "/index.html"));
+    res.sendFile(path.join(__dirname + "/index.html"));
 });
 
 var directory;
 fs.readFile("./directory.json", "utf8", function(err, data) {
-	directory = JSON.parse(data);
-	if (err) {
-		throw err;
-	}
+    directory = JSON.parse(data);
+    if (err) {
+        throw err;
+    }
 });
 
 app.get("/search", function(req, res) {
-	var results = directory.reduce(function(acc, file) {
-		if (file.tags.indexOf(req.query.q) !== -1) {
-			acc.push({
-				id: file.id,
-				title: file.title,
-				thumb: "/public/images/".concat(file.thumb),
-				price: file.price
-			});
-		}
-		return acc;
-	}, []);
-	res.send(results);
+    if (!req.query.q) {
+        var results = directory.reduce(function(acc, file) {
+            acc.push({
+                id: file.id,
+                title: file.title,
+                thumb: "/public/images/".concat(file.thumb),
+                price: file.price
+            });
+            return acc;
+        }, []);
+        res.send(results)
+        return
+    }
+    var results = directory.reduce(function(acc, file) {
+        if (file.tags.indexOf(req.query.q) !== -1) {
+            acc.push({
+                id: file.id,
+                title: file.title,
+                thumb: "/public/images/".concat(file.thumb),
+                price: file.price
+            });
+        }
+        return acc;
+    }, []);
+    res.send(results);
 });
 
 app.use("/node_modules", express.static(path.join(__dirname, "node_modules")));
 app.use("/public", express.static(path.join(__dirname, "public")));
 
 if (process.env.NODE_ENV !== "production") {
-	require("reload")(app);
+    require("reload")(app);
 }
 
 var port = process.env.PORT || 3000;
-server.listen(port, function () {
-	console.log("Listening on port ".concat(port));
+server.listen(port, function() {
+    console.log("Listening on port http://localhost:".concat(port));
 });
